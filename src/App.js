35c6712b25417f55d998c5446fe80e52a9a1fbdd
Will <28814921+wkey1980@users.noreply.github.com>
2021-06-20 10:50:06 +0100

    Update project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login/Login";
+import Header from "./components/header/Header";
 import GlobalStyle from "./globalStyles";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header />
       <Router>
         <Switch>
           <Route exact path="/">
